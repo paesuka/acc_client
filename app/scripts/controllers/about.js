90c2605b27cad8e55d8b79984d1d93fa57068d7c
Pascal <p.kaufmann.dev@gmail.com>
2016-05-30 18:52:21 +0800

    scaffold

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name accClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the accClientApp
+ */
+angular.module('accClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
