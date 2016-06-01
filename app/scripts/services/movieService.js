eeb8f9991bc263ab3bf027c8fd598f7d76ca259a
Pascal <p.kaufmann.dev@gmail.com>
2016-06-01 08:31:17 +0800

    reroute to own movie database

--- app/scripts/services/movieService.js
+++ app/scripts/services/movieService.js
@@ -10,8 +10,8 @@
 angular.module('accClientApp')
   .service('MovieService', function($http) {
     this.findAll = function() {
-      return $http.get('https://demo2697834.mockable.io/movies').then(function (response) {
-        return response.data.entries;
+      return $http.get('http://localhost:9000/api/v0/movies').then(function (response) {
+        return response.data;
       });
     };
   });
