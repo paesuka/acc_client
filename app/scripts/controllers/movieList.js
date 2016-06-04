3d47732f5bb9e5232ce7a10226c5dcaab1eaafb4
Pascal <p.kaufmann.dev@gmail.com>
2016-06-04 08:25:00 +0800

    refactoring name for movie list

---
+++ app/scripts/controllers/movieList.js
@@ -0,0 +1,97 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name accClientApp.controller:MovieListCtrl
+ * @description
+ * # MovieListCtrl
+ * Controller of the accClientApp
+ */
+angular.module('accClientApp')
+  .controller('MovieListCtrl', ['$window', '$scope', '$uibModal', 'MovieService', 'WatchHistoryService',
+    function($window, $scope, $uibModal, MovieService, WatchHistoryService) {
+
+      function isMobileScreen() {
+        return $window.innerWidth <= 550;
+      }
+
+      $scope.mobile = isMobileScreen();
+      var movieContentIndex = 0;
+      var currentMovieIndex = 0;
+      var modalShowing = false;
+
+      MovieService.findAll().then(function(data) {
+        $scope.movies = data;
+      }, function(data) {
+        console.log('error: ' + data);
+      });
+
+      // configuration for the slick movie carousel
+      $scope.slickConfig = {
+        enabled: true,
+        draggable: true,
+        infinite: true,
+        variableWidth: true,
+        autoplay: false,
+        arrows: false,
+        slidesToShow: 10,
+        slidesToScroll: 1,
+        method: {},
+        event: {
+          init: function() {
+            currentMovieIndex = 0;
+          },
+          beforeChange: function(event, slick, currentSlide, nextSlide) {
+            currentMovieIndex = nextSlide;
+          }
+        }
+      };
+
+      // handle keypress events fired from rootscope
+      $scope.$on('keydown:13', function() {
+        executeMovieSelection($scope.playMovie, $scope.movies[currentMovieIndex]);
+      });
+      $scope.$on('keydown:39', function() {
+        executeMovieSelection($scope.slickConfig.method.slickNext);
+      });
+      $scope.$on('keydown:37', function() {
+        executeMovieSelection($scope.slickConfig.method.slickPrev);
+      });
+
+      //bind window resize to change movie list layout
+      angular.element($window).bind('resize', function() {
+        if ($scope.mobile !== isMobileScreen()) {
+          $scope.mobile = !$scope.mobile;
+          $scope.$apply();
+        }
+      });
+
+      // open and wait for modal to play movie
+      $scope.playMovie = function(movieItem) {
+        modalShowing = true;
+        var uibModalInstance = $uibModal.open({
+          templateUrl: 'views/moviePlayer.html',
+          controller: 'MoviePlayerCtrl',
+          resolve: {
+            movieContent: function() {
+              return movieItem.contents[movieContentIndex];
+            }
+          }
+        });
+        uibModalInstance.result.then(function() {}, function() {
+          modalShowing = false;
+          WatchHistoryService.addWatchedMovie(movieItem).then(function() {},
+            function(data) {
+              console.log('error: failed to persist new watch item ' + data);
+            });
+        });
+      };
+
+      // disable keyboard controls when modal is showing or on mobile
+      function executeMovieSelection(func, param) {
+        if (!modalShowing && !$scope.mobile && $scope.slickConfig.enabled) {
+          func(param);
+        }
+      }
+    }
+  ]);
---
+++ test/spec/controllers/movieList.js
@@ -0,0 +1,32 @@
+'use strict';
+
+describe('Controller: MovieListCtrl', function() {
+
+  // load the controller's module
+  beforeEach(module('accClientApp'));
+
+  var movieListCtrl, scope;
+  var promiseValue = 1;
+
+  // Initialize the controller and mocks for scope and MovieService
+  beforeEach(inject(function($controller, $q, $rootScope) {
+    var deferred = $q.defer();
+    var promise = deferred.promise;
+    deferred.resolve(promiseValue);
+    scope = $rootScope.$new();
+    movieListCtrl = $controller('MovieListCtrl', {
+      $scope: scope,
+      MovieService: {
+        findAll: function() {
+          return promise;
+        }
+      }
+    });
+  }));
+
+  it('should set movies to promise when call finished', function() {
+    expect(scope.movies).toBe(undefined);
+    scope.$apply();
+    expect(scope.movies).toBe(promiseValue);
+  });
+});
