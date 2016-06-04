72c94becde1c91ce9dd79b3f3ec37d7fdf77d314
Pascal <p.kaufmann.dev@gmail.com>
2016-06-04 10:00:40 +0800

    cleanup

--- app/scripts/services/movieService.js
+++ app/scripts/services/movieService.js
@@ -9,6 +9,7 @@
  */
  angular.module('accClientApp')
    .service('MovieService', ['$http', 'config', function($http, config) {
+
     this.findAll = function() {
       return $http.get(config.apiMoviesUrlDev).then(function (response) {
         return response.data;
--- app/scripts/services/watchHistoryService.js
+++ app/scripts/services/watchHistoryService.js
@@ -9,6 +9,7 @@
  */
  angular.module('accClientApp')
    .service('WatchHistoryService', ['$http', '$rootScope', 'config', function($http, $rootScope, config) {
+
     this.findByUserId = function(userId) {
       return $http.get(config.apiWatchhistoryUrlDev + userId).then(function(response) {
         return response.data;
@@ -16,6 +17,7 @@
         console.log('error: failed to retrieve watchHistory' + response);
       });
     };
+
     this.addWatchedMovie = function(movieItem) {
       var watchItem = {
         title: movieItem.title,
